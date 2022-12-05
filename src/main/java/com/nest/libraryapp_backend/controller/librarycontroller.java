da6031acdd263a801c6605ea37642f2e1de27f0f
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-05 22:18:40 +0530

    library

--- src/main/java/com/nest/libraryapp_backend/controller/librarycontroller.java
+++ src/main/java/com/nest/libraryapp_backend/controller/librarycontroller.java
@@ -1,4 +1,15 @@
 package com.nest.libraryapp_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class librarycontroller {
+
+    @PostMapping("/")
+    public String Adminlogin()
+    {
+        return " welcome to admin login page";
+    }
 }
